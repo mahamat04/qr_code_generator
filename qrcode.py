241b73f1987273a28a97e76b223f350cc459d3ef
Mahamat04 <mahamatalhabib224@gmail.com>
2022-12-25 02:21:48 +0100

    Changing module

--- qrcode.py
+++ qrcode.py
@@ -1,17 +1,11 @@
-import qrcode
-
-def generate_qrcode(text):
-    code = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=12,
-        border=5,
-    )
-
-    code.add_data(text)
-    code.make(fit=True)
-    img = code.make_image(fill_color="gray", back_color="white")
-    img.save("qrcode.png")
+import pyqrcode
+import png
+from pyqrcode import QRCode
+def generate_qrcode(url):
+    code = pyqrcode.create(url)
+    code.png('test.png', scale=8)
 
 
-generate_qrcode('This is beta test!')
+url = input('Enter your URL: ')
+generate_qrcode(url)
+print('Work ended look at the png (test.png) file!')
